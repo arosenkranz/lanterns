f2e479af45e8cf90b7e91a4d303ae9c1c8419438
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-02-27 18:23:48 -0500

    working on tweens

--- client/src/containers/web/MainView.js
+++ client/src/containers/web/MainView.js
@@ -5,9 +5,24 @@ import io from 'socket.io-client';
 import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js';
 import random from 'canvas-sketch-util/random';
-import {lerp, pingPong, inverseLerp} from 'canvas-sketch-util/math';
+import { lerp, pingPong, inverseLerp } from 'canvas-sketch-util/math';
+import * as OBJLoader from 'three-obj-loader';
+OBJLoader(THREE);
+
+const OrbitControls = require('three-orbit-controls')(THREE);
 
 const socket = io('http://localhost:3001');
+
+const styles = {
+  position: 'absolute',
+  zIndex: 9999,
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  width: '100%',
+  height: '100%'
+};
 
 class MainView extends Component {
   state = {
@@ -48,30 +63,50 @@ class MainView extends Component {
   initThreeScene = () => {
     this.meshes = [];
     this.time = 0;
+    this.mesh = '';
+    this.THREE = THREE;
 
-    console.log(this.mount);
     const width = window.innerWidth;
     const height = window.innerHeight;
-    console.log(width, height);
     //ADD SCENE
-    this.scene = new THREE.Scene();
+    this.scene = new this.THREE.Scene();
 
-    this.camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 1000);
+    this.camera = new this.THREE.PerspectiveCamera(40, width / height, 0.1, 1000);
     this.camera.position.set(0, 0.5, 9);
     this.camera.lookAt(this.scene.position);
 
     //ADD RENDERER
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setClearColor(0x140b33, 1);
+    this.renderer = new this.THREE.WebGLRenderer({ antialias: true, alpha: true });
+    // this.renderer.setClearColor('#fff');
     this.renderer.setSize(width, height);
     this.mount.appendChild(this.renderer.domElement);
 
     //ADD CUBE
-    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 1);
+    this.geometry = new this.THREE.BoxGeometry(0.2, 0.2, 1);
+
+    this.logo = THREE.ImageUtils.loadTexture('/assets/images/mark-conference-logo-black.png');
     this.material = new THREE.MeshStandardMaterial({
-      metalness: 0,
+      // map: this.logo,
+      opacity: 1,
       roughness: 1,
-      color: '#ba8f44'
+      transparent: false,
+      metalness: 0,
+      emissive: '#C9AF5B',
+      emissiveIntensity: 0
+    });
+
+    this.model = new this.THREE.OBJLoader().load('/assets/models/lantern.obj', object => {
+      // For any meshes in the model, add our material.
+      object.traverse(node => {
+        if (node.isMesh) node.material = this.material;
+      });
+
+      this.mesh = object;
+      this.mesh.scale.set(.05, .05, .05);
+      console.log(this.mesh);
+      this.createLantern({ name: 'one' });
+      // this.createLantern({ name: 'two' });
+      // this.createLantern({ name: 'three' });
     });
 
     // for perspective
@@ -84,14 +119,17 @@ class MainView extends Component {
     const ambientlight = new THREE.AmbientLight(0x404040);
     this.scene.add(ambientlight);
 
-    var spotLight = new THREE.SpotLight();
+    const spotLight = new THREE.SpotLight();
     spotLight.position.set(0, 80, 30);
     spotLight.castShadow = true;
     this.scene.add(spotLight);
 
-    this.createLantern({ name: 'one' });
-    this.createLantern({ name: 'two' });
-    this.createLantern({ name: 'three' });
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
+
+    this.controls = new OrbitControls(this.camera);
+    this.controls.enableDamping = false;
+    this.controls.autoRotate = false;
 
     this.start();
   };
@@ -101,34 +139,40 @@ class MainView extends Component {
     const parent = new THREE.Object3D();
     parent.name = lanternInfo.name;
     this.scene.add(parent);
+    const lantern = this.mesh.clone();
+    // const cube = new THREE.Mesh(this.geometry, this.material);
+    lantern.position.set(random.range(-3.5, 3.5), -10, random.range(-6, 9));
+    lantern.castShadow = true;
+    lantern.receiveShadow = true;
 
-    const cube = new THREE.Mesh(this.geometry, this.material);
-    cube.position.set(random.range(-3.5, 3.5), -10, random.range(-6, 9));
-    console.log(cube);
-    cube.castShadow = true;
-    cube.receiveShadow = true;
+    lantern.originalPosition = lantern.position.clone();
 
-    cube.originalPosition = cube.position.clone();
-
-    const position = cube.position;
-    const target = { x: cube.position.x, y: 5, z: cube.position.z };
+    const position = lantern.position;
+    const rotation = lantern.rotation;
+    const target = { x: lantern.position.x, y: 5, z: lantern.position.z };
 
     const tween = new TWEEN.Tween(position).to(target, 10000);
 
+    const tween2 = new TWEEN.Tween({rotation: 0}).to({rotation: 45}, 2000);
+    const tween3 = new TWEEN.Tween(lantern.rotation).to({rotation : -45}, 2000);
+    tween2.chain(tween3);
+    tween3.chain(tween2);
+
     const randomNum = Math.random();
     tween.onUpdate(function() {
-      cube.rotation.z = lerp(-10.25, 10.25, Math.random());
+      // cube.rotation.z = lerp(-10.25, 10.25, Math.random());
 
       // cube.position.x = position.x;
-      cube.position.y = position.y;
+      lantern.position.y = position.y;
     });
 
     tween.repeat(Infinity);
+    tween2.repeat(Infinity);
 
-    parent.add(cube);
+    parent.add(lantern);
     tween.start();
 
-    this.meshes.push(cube);
+    this.meshes.push(lantern);
   };
 
   start = () => {
@@ -142,9 +186,8 @@ class MainView extends Component {
   };
 
   animate = () => {
-    // this.cube.rotation.x += 0.01;
-    // this.cube.rotation.y += 0.01;
     this.time += 1;
+    this.controls.update();
     this.renderScene();
     this.frameId = window.requestAnimationFrame(this.animate);
     TWEEN.update();
@@ -181,7 +224,7 @@ class MainView extends Component {
 
     return (
       <div
-        style={{ minWidth: '100vh', minHeight: '100vh' }}
+        style={{ minWidth: '100vh', minHeight: '100vh', ...styles }}
         ref={mount => {
           this.mount = mount;
         }}
