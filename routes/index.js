29328748df20a7a09804a2f35da2e9bfa0869b9e
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-03-01 23:22:18 -0500

    added wildcard

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.get('/', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build'));
 });
 
